e436b2339c32a76c4db398d90c80cbcef2d4beec
jon <jon@tpad.home>
2019-06-07 16:30:34 +0100

    added comments & updated rate limit counters / sleep variables

--- academic_chat.py
+++ academic_chat.py
@@ -4,6 +4,7 @@ from time import sleep
 import datetime
 import sys
 
+# insert Twitter Dev App security keys and tokens
 consumer_key        = "***************************"
 consumer_secret     = "**************************************************"
 access_token        = "**************************************************"
@@ -14,28 +15,33 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-search1  = ("#phdchat -filter:retweets AND -filter:replies")
-search2  = ("#acwri OR #ecrchat OR #phdforum OR #phdadvice OR #phdlife OR #phdweekend OR #gradschool OR #phdgang OR #phdhelp OR #planningyourphd -filter:retweets AND -filter:replies")
+# set search content, we prioritize tweets that have tagged Academic Chatter directly (search 1)
+search1  = ("@AcademicChatter OR #academicchatter OR #acchat -filter:retweets AND -filter:replies")
+
+# if we don't find tweets that included our tag then we search for general academic twitter hashtags 
+search2  = ("#phdchat OR #academictwitter OR #acwri OR #ecrchat OR #phdforum OR #phdadvice OR #phdlife OR #phdweekend OR #gradschool OR #phdgang OR #phdhelp OR #planningyourphd -filter:retweets AND -filter:replies")
 
 running = True
 while running == True:
 
-    x=0
-    y=0
+    RateLimitCounter1=0
+    RateLimitCounter2=0
+    # first search for tweets containing Academic Chatter tags (Search 1)
     for tweet in tweepy.Cursor(api.search,
                                q=search1,
                                result_type="recent",
                                lang='en').items(1):
 
-        # users that we don't want to share the tweets of (spam/abuse/troll)
+        # if found tweet was shared by one of these users (spam/abuse/troll), then don't share (break loop) 
 	if tweet.user.screen_name == "@user-to-block1" or tweet.user.screen_name == "@user-to-block2":
 		print (tweet.user.screen_name)
 		print ("avoiding spam users")
  		break
 
-        if x==0:
+        if RateLimitCounter1==0:
 
-	    print ("tweet content: ", tweet.text)
+            # information for output logs
+	    print ("tweet content: ", tweet.text) 
             print ("length of original tweet: ", len(tweet.text))
  
             # if found tweet contains any of these strings, don't share (block inappropriate content)
@@ -43,10 +49,10 @@ while running == True:
 	        print "spam found"
 	        break
 
-            y+=1
+            RateLimitCounter2+=1
             try:
 		tweet.retweet()
-		x+=1
+		RateLimitCounter1+=1
 
 	    except tweepy.TweepError as e:
 		print (e)
@@ -55,31 +61,34 @@ while running == True:
             except StopIteration:
 		break
 
-        if x==0:
-            y+=1
+        if RateLimitCounter1==0:
+            RateLimitCounter2+=1
+            # second search for tweets containing general academic twitter hashtags (search 2)
             for tweet in tweepy.Cursor(api.search,
                                        q=search2,
                                        result_type="recent",
                                        lang='en').items(1):
-
+                
+                # if found tweet was shared by one of these users (spam/abuse/troll), then don't share (break loop)
 	        if tweet.user.screen_name == "@user-to-block1" or tweet.user.screen_name == "@user-to-block2":
 		    print (tweet.user.screen_name)
 		    print ("avoiding spam users")
  		    break
 
-        	if x==0:
-
+        	if RateLimitCounter1==0:
+                    # useful for output logs
 	   	    print ("tweet content: ", tweet.text)
                     print ("length of original tweet: ", len(tweet.text))
  
+                    # if found tweet contains any of these strings, don't share (block inappropriate content)
 	   	    if "#inappropriate-word1" in tweet.text or "#inappropriate-word2" in tweet.text:
 	                print "spam found"
 			break
                 try:
                     tweet.favorite()
 		    tweet.retweet()
-		    x+=1
-                    y+=1
+		    RateLimitCounter1+=1
+                    RateLimitCounter2+=1
 
 		except tweepy.TweepError as e:
 		    print (e)
@@ -88,10 +97,15 @@ while running == True:
                 except StopIteration:
                     break
                     
-
-    if y == 2:
-        time.sleep(360)
-    elif y==3:
-        time.sleep(540)
+    # 2 searches but found nothing to share
+    if RateLimitCounter2==2:
+        time.sleep(480)
+        print("Did 2 searches but found nothing to share - sleeping for 480 seconds (8 mins)")
+    # 2 searches and 1 share on the second search
+    elif RateLimitCounter2==3:
+        time.sleep(600)
+        print("Did 2 searches but found something to share on the second search - sleeping for 600 seconds (10 mins)")
+    # 1 search and 1 share
     else:
-        time.sleep(180)
+        time.sleep(600)
+        print("Did 1 search and found tweet to share that tagged the account directly - sleeping for 600 seconds (10 mins)")
